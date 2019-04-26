07cb91cd3854eefdcd0169b90a68509ff588cdf8
David Gasper <dmgasper@green.workstation.cass.oregonstate.edu>
2019-04-26 14:19:08 -0700

    Update cookstyle

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,20 +5,12 @@ ChefSpec::Coverage.start! { add_filter 'osl-varnish' }
 
 CENTOS_7 = {
   platform: 'centos',
-<<<<<<< HEAD
   version: '7.4.1708',
-=======
-  version: '7.4.1708'
->>>>>>> d31a4c49b047fc8a9a3d60b41786421899b97436
 }.freeze
 
 CENTOS_6 = {
   platform: 'centos',
-<<<<<<< HEAD
   version: '6.9',
-=======
-  version: '6.9'
->>>>>>> d31a4c49b047fc8a9a3d60b41786421899b97436
 }.freeze
 
 ALL_PLATFORMS = [
