e753dcfcd31f4989df733e2092f2ef01a1a39630
Catherine Stevns <stevenscatherine2@gmail.com>
2019-05-02 15:18:31 -0700

    Fixed style issues

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ source_url       'https://github.com/osuosl-cookbooks/osl-varnish'
 description      'OSL wrapper cookbook for varnish'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
-chef_version	 '>= 13.8.5' if respond_to?(:chef_version)
+chef_version     '>= 13.8.5' if respond_to?(:chef_version)
 
 depends          'varnish'
 depends          'firewall'
